bbb4e1ddbb916d5cebc1fa80d91cf13314d56205
MagicMicky <micky0304@gmail.com>
2017-10-11 17:03:50 +0200

    configurable waiting period

--- README.md
+++ README.md
@@ -4,9 +4,9 @@ This is a small software that does the following:
 
 The goal was to monitor if docker shutdown's were handled gracefully when a machine stops.
 
-The url to query is configurable. By default it would query Google, but it can be used to query a custom endpoint so that you are notified on a distant server of the shutdown happening (ie: if the machine hosting the container is supposed to be destroyed)
+The url to query and the waiting period are configurable. By default it would query Google, but it can be used to query a custom endpoint so that you are notified on a distant server of the shutdown happening (ie: if the machine hosting the container is supposed to be destroyed)
 
 ```
-docker run -d MagicMicky/signals-to-http http://mycustomurl/
+docker run -d MagicMicky/signals-to-http  25 http://mycustomurl/
 ```
 
--- signals-to-http.go
+++ signals-to-http.go
@@ -6,8 +6,11 @@ import "os/signal"
 import . "time"
 import "net/http"
 import "strings"
+import "strconv"
+const DEFAULT_URL string ="http://google.com"
+const DEFAULT_SECS int =9
 
-func getGoogle(url string) {
+func queryUrl(url string) {
   fmt.Printf("getting %s\n", url)
   resp, err := http.Get(url)
 	if resp != nil {
@@ -18,19 +21,33 @@ func getGoogle(url string) {
   }
 }
 
-func getUrl(args []string) string {
-  url := strings.Join(args, "")
-  if len(url) == 0 {
-    return "http://google.com"
+func getUrl() string {
+  ret_url := DEFAULT_URL
+  if len(os.Args) > 2 {
+    url := strings.Join(os.Args[2:], "")
+    if len(url) > 0 {
+      ret_url=url
+    }
   }
-  return url
+  return ret_url
+}
+
+func getNbSec() int {
+  ret_secs:=DEFAULT_SECS
+  if len(os.Args) > 1 && os.Args[1] != "" {
+    res, err := strconv.Atoi(os.Args[1])
+    if err == nil {
+      ret_secs=res
+    }
+  }
+  return ret_secs
 }
 
 func main() {
-  delay := 9 * Second
+  delay := Duration(getNbSec()) * Second
   sigs := make(chan os.Signal, 1)
   done := make(chan bool, 1)
-  args := getUrl(os.Args[1:])
+  args := getUrl()
 
   signal.Notify(sigs)
 	go func() {
@@ -38,7 +55,7 @@ func main() {
 		fmt.Println()
     fmt.Printf("Received signal %s, sleeping for %d secs\n", sig, int(delay.Seconds()))
     Sleep(delay)
-		getGoogle(args)
+		queryUrl(args)
 		done <- true
 	}()
 
