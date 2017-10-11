45748e462420d86f32f96ff5d61f014d46d51dbb
MagicMicky <micky0304@gmail.com>
2017-10-11 17:27:30 +0200

    go fmt

--- signals-to-http.go
+++ signals-to-http.go
@@ -7,54 +7,55 @@ import . "time"
 import "net/http"
 import "strings"
 import "strconv"
-const DEFAULT_URL string ="http://google.com"
-const DEFAULT_SECS int =9
+
+const DEFAULT_URL string = "http://google.com"
+const DEFAULT_SECS int = 9
 
 func queryUrl(url string) {
-  fmt.Printf("getting %s\n", url)
-  resp, err := http.Get(url)
+	fmt.Printf("getting %s\n", url)
+	resp, err := http.Get(url)
 	if resp != nil {
-    fmt.Println(resp)
-    defer resp.Body.Close()
-  } else {
-    fmt.Println(err)
-  }
+		fmt.Println(resp)
+		defer resp.Body.Close()
+	} else {
+		fmt.Println(err)
+	}
 }
 
 func getUrl() string {
-  ret_url := DEFAULT_URL
-  if len(os.Args) > 2 {
-    url := strings.Join(os.Args[2:], "")
-    if len(url) > 0 {
-      ret_url=url
-    }
-  }
-  return ret_url
+	ret_url := DEFAULT_URL
+	if len(os.Args) > 2 {
+		url := strings.Join(os.Args[2:], "")
+		if len(url) > 0 {
+			ret_url = url
+		}
+	}
+	return ret_url
 }
 
 func getNbSec() int {
-  ret_secs:=DEFAULT_SECS
-  if len(os.Args) > 1 && os.Args[1] != "" {
-    res, err := strconv.Atoi(os.Args[1])
-    if err == nil {
-      ret_secs=res
-    }
-  }
-  return ret_secs
+	ret_secs := DEFAULT_SECS
+	if len(os.Args) > 1 && os.Args[1] != "" {
+		res, err := strconv.Atoi(os.Args[1])
+		if err == nil {
+			ret_secs = res
+		}
+	}
+	return ret_secs
 }
 
 func main() {
-  delay := Duration(getNbSec()) * Second
-  sigs := make(chan os.Signal, 1)
-  done := make(chan bool, 1)
-  args := getUrl()
+	delay := Duration(getNbSec()) * Second
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	args := getUrl()
 
-  signal.Notify(sigs)
+	signal.Notify(sigs)
 	go func() {
 		sig := <-sigs
 		fmt.Println()
-    fmt.Printf("Received signal %s, sleeping for %d secs\n", sig, int(delay.Seconds()))
-    Sleep(delay)
+		fmt.Printf("Received signal %s, sleeping for %d secs\n", sig, int(delay.Seconds()))
+		Sleep(delay)
 		queryUrl(args)
 		done <- true
 	}()
